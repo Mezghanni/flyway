3cc3675bdc8cfa14cc93d9c6cef6cb5aa42c2bc7
Mezghanni <med.said.mezghanni@gmail.com>
2020-09-10 00:59:49 -0400

    test SQL/Java migration

--- src/main/java/com/example/flyway/FlywayApplication.java
+++ src/main/java/com/example/flyway/FlywayApplication.java
@@ -1,12 +1,32 @@
 package com.example.flyway;
 
+import org.flywaydb.core.api.configuration.FluentConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 public class FlywayApplication {
+
+	@Configuration
+	public class FlywayConfiguration {
+		@Bean
+		public FlywayConfigurationCustomizer customizeLicense(
+				@Value("${my-app.flyway.license}") String license) {
+			return new FlywayConfigurationCustomizer() {
+
+				@Override
+				public void customize(FluentConfiguration configuration) {
+					configuration.licenseKey(license);
+				}
+			};
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlywayApplication.class, args);
